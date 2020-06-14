23281dacd765590cdc33e372ad3f58e33c8189f7
Douglas Honda <doug.honda2@gmail.com>
2020-06-14 19:27:32 -0300

    created database using sqlite3

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const server = express();
 
+//Pegar o banco de dados
+const db = require("./database/db")
+
 //Configurar pasta pública
 server.use(express.static("public"));
 
@@ -21,7 +24,19 @@ server.get("/create-point", (req, res) => {
 });
 
 server.get("/search", (req, res) => {
-  return res.render("search-results.html");
+  //Pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function (err, rows) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Aqui estão seus registros:");
+    console.log(rows);
+
+    const total = rows.length
+    
+    //Mostrar a página HTML com os dados do banco de dados
+    return res.render("search-results.html", {places: rows, total});
+  });
 });
 
 //Ligar o servidor
